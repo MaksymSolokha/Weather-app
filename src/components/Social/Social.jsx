4029d1750a69a4cf8db7ae80f0c5a83bd2b96b6d
Максим Солоха <maksimsoloha@MacBook-Pro-Maksim.local>
2022-11-30 20:29:28 +0200

    small fix

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -5,14 +5,17 @@ import instagram from '../../assets/img/instagramicon.svg';
 import github from '../../assets/img/github.svg';
 
 const LINKS = [{
+	name: 'github',
 	href: 'https://github.com/maksimsolokha',
 	img: github,
 },
 {
+	name: 'linkedin',
 	href: 'https://www.linkedin.com/in/maksim-solokha-270933254/',
 	img: linkedin,
 },
 {
+	name: 'instagram',
 	href: 'https://www.instagram.com/solokha.m_/',
 	img: instagram,
 }];
@@ -24,7 +27,7 @@ function Social() {
 				{LINKS.map((link) => (
 					<li key={link.href} className="social__list__item">
 						<a href={link.href} target="_blank" rel="noreferrer">
-							<img src={link.img} alt="" />
+							<img src={link.img} alt={link.name} />
 						</a>
 					</li>
 				))}
